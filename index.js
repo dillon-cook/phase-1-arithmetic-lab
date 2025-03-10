f7384238361a7f786eeedd129732d3fa0d49939f
dilloncook <cookdillon876@gmail.com>
2025-03-10 14:24:21 +0300

    update

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+let multiply = 62;
+let num1 = 31;
+let num2 = 2;
+const random = Math.floor(Math.random() * 100) + 1;
+const num3 = 16;
+const num4 = 6;
+let mod = num3 % num4; 
+let max = Math.max(1, 2, 3, 20)
+
+
+
+
+
+ 
+ 
